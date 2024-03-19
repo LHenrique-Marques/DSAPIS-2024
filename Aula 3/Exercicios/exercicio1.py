4b6d14fbf558a63e476d5a94f0a6a612749a769a
Henrique Marques <henrique.marquesvj@gmail.com>
2024-03-19 20:48:44 -0300

    arrumando

--- Aula 3/Exercicios/exercicio1.py
+++ Aula 3/Exercicios/exercicio1.py
@@ -7,8 +7,10 @@ class Produto():
 
     def __str__(self):
         return f"""
+        ID:{self.id}
         Nome:{self.nome} 
-        ID:{self.id}"""
+        Categoria:{self.nome}
+        """
 
     def get_id(self):
         return self.id
@@ -43,6 +45,7 @@ class listas():
         for iten in self.itens:
             if iten.get_id() == id_buscada:
                 print(iten)
+                return iten
     
     def pesquisa_categoria(self,categoria):
         categorias = []
@@ -50,9 +53,11 @@ class listas():
             if iten.get_categoria() == categoria:
                 categorias.append(iten)
 
-        print("Produtos:")
         for categoria in categorias:
-            print(f"""{categoria},""")
+            print(f"""{categoria}""")
+        
+        if len(categorias) == 0:
+            print("Categoria Vazia..")
     
     def muda_nome(self,id_buscada,novo_nome):
         for iten in self.itens:
@@ -60,7 +65,56 @@ class listas():
                 iten.set_nome(novo_nome)
 
 
-lista1 = listas()
-lista1.adiciona_item("Refrigerante","Bebidas")
-lista1.adiciona_item("Pizza","Lanches")
-lista1.adiciona_item("Chopp","Bebidas")
+def menu():
+    return("""
+    -----------------------MENU-----------------------
+    0 - Fecha o Programa
+    1 - Adiciona Item
+    2 - Remove Item
+    3 - Mostra Lista
+    4 - Pesquisa por ID
+    5 - Pesquisar por Categoria
+    6 - Mudar nome do produto
+    """)
+def inicializa():
+    lista1 = listas()
+    lista1.adiciona_item("Refrigerante","Bebidas")
+    lista1.adiciona_item("Pizza","Lanches")
+    lista1.adiciona_item("Chopp","Bebidas")
+    lista1.adiciona_item("Hamburguer","Lanches")
+    while True:
+        print(menu())
+        try:
+            escolha = int(input("Escolha: "))
+            
+            if escolha == 0:
+                break
+            
+            if escolha == 1:
+                lista1.adiciona_item(input("Digite o nome do novo produto: "),input("Digite a Categoria do novo produto: "))
+            
+            if escolha == 2:
+                lista1.mostra_itens()
+                try:
+                    lista1.deleta_iten(int(input("Digite a ID do produto que deseja Remover: ")))
+                except:
+                    print("ID invalido.... tente novamente.")
+
+            if escolha == 3:
+                lista1.mostra_itens()
+            
+            if escolha == 4:
+                try:
+                    p = lista1.procura_id(int(input("Digite a ID do produto que você procura: ")))
+                    if p == None:
+                        print("ID não encontrada...")
+                except:
+                    print("Ops... Não encontrei esta ID, tente novamente.")
+            
+            if escolha == 5:
+                lista1.pesquisa_categoria(input("Digite o nome da Categoria que você deseja buscar: "))
+        except:
+            print("Opção invalida... Tente novamente...")
+
+
+inicializa()
