aaedaad7156a9e54371892a818cd571ff3f4831b
Henrique Marques <henrique.marquesvj@gmail.com>
2024-03-26 19:52:20 -0300

    adicionando main.js

---
+++ Projetos/crud-produtos/main.js
@@ -0,0 +1,22 @@
+
+let listaProdutos = [
+    {
+        id:1,
+        nome: "Feijão",
+        categoria: "Alimento",
+        preco: 5.80
+    },
+    {
+        id: 2,
+        nome: "Leite",
+        categoria: "Bebida",
+        preco: 3.45
+    }
+];
+function listar (){
+    return listaProdutos;
+}
+
+function main (){
+
+}
